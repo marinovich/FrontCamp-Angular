a6046523bf8f312d6d48dfaf66d6fe426f02ec37
Aliaksandr Marynovich <aliaksandr_marynovich@epam.com>
2019-02-25 03:52:06 +0300

    Auto-generated commit

--- runtime.js
+++ runtime.js
@@ -136,7 +136,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
+/******/ 	__webpack_require__.p = "https://marinovich.github.io/FrontCamp-Angular/";
 /******/
 /******/ 	var jsonpArray = window["webpackJsonp"] = window["webpackJsonp"] || [];
 /******/ 	var oldJsonpFunction = jsonpArray.push.bind(jsonpArray);
